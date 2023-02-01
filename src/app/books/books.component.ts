69581c93b6e45acd9a1be3748da9ff0c7664abb4
Coel Allen <coelallen@gmail.com>
2023-02-01 15:11:49 -0700

    implementing service layer

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,53 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Book } from '../types/Book';
+import { BooksService } from './books.service';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css'],
 })
-export class BooksComponent {
-  books: Book[] = [
-    {
-      name: 'clean code',
-      author: 'robert martin',
-      image:
-        'https://images-na.ssl-images-amazon.com/images/I/41zoxjP9lcL._SX323_b01,204,203,200_.jpg',
-      amount: 12.95,
-    },
-    {
-      name: 'pragmatic programmer',
-      author: 'david thomas',
-      image: 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg',
-      amount: 14.95,
-    },
-    {
-      name: 'clean code',
-      author: 'robert martin',
-      image:
-        'https://images-na.ssl-images-amazon.com/images/I/41zoxjP9lcL._SX323_b01,204,203,200_.jpg',
-      amount: 12.95,
-    },
-    {
-      name: 'pragmatic programmer',
-      author: 'david thomas',
-      image: 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg',
-      amount: 14.95,
-    },
-    {
-      name: 'clean code',
-      author: 'robert martin',
-      image:
-        'https://images-na.ssl-images-amazon.com/images/I/41zoxjP9lcL._SX323_b01,204,203,200_.jpg',
-      amount: 12.95,
-    },
-    {
-      name: 'pragmatic programmer',
-      author: 'david thomas',
-      image: 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg',
-      amount: 14.95,
-    },
-  ];
+export class BooksComponent implements OnInit {
+  constructor(private booksService: BooksService) {}
+
+  ngOnInit(): void {
+    this.books = this.booksService.getBooks();
+  }
+
+  books: Book[] = [];
 
   cart: Book[] = [];
   addToCart(event: Book) {
---
+++ src/app/books/books.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BooksService {
+  constructor() {}
+  getBooks() {
+    return [
+      {
+        name: 'clean code',
+        author: 'robert martin',
+        image:
+          'https://images-na.ssl-images-amazon.com/images/I/41zoxjP9lcL._SX323_b01,204,203,200_.jpg',
+        amount: 12.95,
+      },
+      {
+        name: 'pragmatic programmer',
+        author: 'david thomas',
+        image: 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg',
+        amount: 14.95,
+      },
+      {
+        name: 'clean code',
+        author: 'robert martin',
+        image:
+          'https://images-na.ssl-images-amazon.com/images/I/41zoxjP9lcL._SX323_b01,204,203,200_.jpg',
+        amount: 12.95,
+      },
+      {
+        name: 'pragmatic programmer',
+        author: 'david thomas',
+        image: 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg',
+        amount: 14.95,
+      },
+      {
+        name: 'clean code',
+        author: 'robert martin',
+        image:
+          'https://images-na.ssl-images-amazon.com/images/I/41zoxjP9lcL._SX323_b01,204,203,200_.jpg',
+        amount: 12.95,
+      },
+      {
+        name: 'pragmatic programmer',
+        author: 'david thomas',
+        image: 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg',
+        amount: 14.95,
+      },
+    ];
+  }
+}
