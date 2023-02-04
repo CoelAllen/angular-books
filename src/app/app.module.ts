ecb523204a4298fc6a8bf13943a0196d1f1cd8f9
Coel Allen <coelallen@gmail.com>
2023-02-03 17:37:00 -0700

    initiating http api calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { BooksModule } from './books/books.module';
 import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent, CartComponent],
-  imports: [BrowserModule, BooksModule, AppRoutingModule, AuthModule],
+  imports: [
+    BrowserModule,
+    BooksModule,
+    AppRoutingModule,
+    AuthModule,
+    HttpClientModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
